aced1c621ffbc4c2033c52eccad3915c91315898
manoj <manojbotta83@gmail.com>
2023-07-26 14:15:41 +0100

    first commit

---
+++ Amigos/BoxingUnBoxing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amigos
+{
+    internal class BoxingUnBoxing
+    {
+        static void Main()
+        {
+            int iii = 100;
+            Object obt = iii;  // implicit conversion or boxing // reference value
+            int j = (int)obt;
+            
+            Console.WriteLine(j);
+            
+            
+            double d = 12.34;
+            int myInt = (int)d; // explicitly converted
+            Console.WriteLine(myInt);
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ Amigos1/TypeCasting1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amigos1
+{
+    internal class TypeCasting1
+    {
+        static void Main()
+        {
+            long quantity1;
+            int quantity2 = 100;
+
+            quantity1 = quantity2; // implicit convertion
+
+
+
+
+
+        
+        
+        }
+
+
+
+    }
+}
